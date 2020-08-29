05eef7d427dea6bea465945058965a1547ec3219
Cristian David Escobar Castillo <cristianescobarcdec@gmail.com>
2020-08-29 15:29:45 -0500

    Lista de publicaciones y diseño login

--- App.tsx
+++ App.tsx
@@ -1,4 +1,25 @@
-import { StatusBar } from 'expo-status-bar';
+import * as React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import ScreenPublicaciones from "./app/screens/Publicaciones";
+import ScreenMain from "./app/screens/Main";
+import LoginScreen from "./app/screens/security/Login";
+
+const Stack = createStackNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Main" component={ScreenMain} />
+        <Stack.Screen name="Publicaciones" component={ScreenPublicaciones} /> 
+        <Stack.Screen name="Login" component={LoginScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+/* import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View, TextInput } from 'react-native';
 
@@ -34,4 +55,4 @@ const styles = StyleSheet.create({
   }
 
 
-});
+}); */
---
+++ app/screens/Main.tsx
@@ -0,0 +1,136 @@
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import {
+    StyleSheet,
+    Text,
+    View,
+    Alert,
+    FlatList,
+    Image,
+    Dimensions,
+    TouchableHighlight,
+} from "react-native";
+
+import SessionNavbar from "./security/Navbar";
+
+export default class Main extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            loading: false,
+            Publicaciones: [],
+            url:
+                'http://192.168.0.100:3000/publicaciones',
+        };
+    }
+
+    componentDidMount = () => {
+        this.getPublicaciones();
+    };
+
+    getPublicaciones = () => {
+        this.setState({ loading: true });
+        fetch(this.state.url)
+            .then((data) => data.json())
+            .then((data) => {
+                this.setState({
+                    publicaciones: data,
+                    loading: false,
+                });
+            })
+            .catch((err) => {
+                //console.log(err);
+                Alert.alert("Error", "Error loading courses.");
+            });
+    };
+
+    render() {
+        const { navigation } = this.props;
+        if (this.state.loading) {
+            return (
+                <View style={styles.dataViewLoading}>
+                    <Text>Cargando Publicaicones... En la pagina del Main</Text>
+                </View>
+            );
+        } else {
+            return (
+                <View style={styles.publicacionesView}>
+                    <SessionNavbar navigation={navigation}></SessionNavbar>
+                    <Text style={{ color: "orange", fontSize: 25 }}>Todas las Publicaciones en el Main</Text>
+                    <FlatList
+                        style={styles.flatList}
+                        data={this.state.publicaciones}
+                        renderItem={({ item }) => (
+                            <View style={styles.publicacionViewContent}>
+                                <TouchableHighlight
+                                 onPress={() => {
+                                  Alert.alert(`Image De la Publicacion: ${item.idPublicacion}`);
+                                }}
+                                >
+                                    <Image
+                                        source={{
+                                            width: 200,
+                                            height: 150,
+                                            uri: `http://192.168.0.100:3000/files/2/${item.idPublicacion}`,
+                                        }}
+                                    />
+                                </TouchableHighlight>
+                                <Text style={styles.publi}>{item.titulo}</Text>
+                                <Text numberOfLines={1}>{item.contenido}</Text>
+                                <Text>Fecha: {item.fecha}</Text>
+                            </View>
+                        )}
+                    ></FlatList>
+                </View>
+            );
+        }
+    }
+}
+
+const styles = StyleSheet.create({
+    publicacionViewContent: {
+        borderColor: "gray",
+        borderWidth: 2,
+        borderRadius: 5,
+        margin: 5,
+    },
+    publi: {
+        fontSize: 18,
+        color: "#ff0000",
+    },
+    publicacionesView: {
+        alignItems: "center",
+        alignContent: "center",
+        flex: 1,
+    },
+    dataViewLoading: {
+        alignItems: "center",
+        alignContent: "center",
+        flex: 1,
+    },
+    button: {
+        alignSelf: "center",
+        alignContent: "flex-start",
+    },
+    itemTitle: {
+        padding: 10,
+        fontSize: 25,
+        height: 44,
+        fontWeight: "bold",
+    },
+    flatList: {
+        alignContent: "center",
+        textAlign: "center",
+        alignSelf: "center",
+    },
+    img: {
+        width: 100,
+        height: 100,
+    },
+    separator: {
+        height: 4,
+        backgroundColor: "black",
+        width: Dimensions.get("window").width / 2,
+    },
+});
