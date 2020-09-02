2e4745f984f595da952b583e8a0662362795653f
Cristian David Escobar Castillo <cristianescobarcdec@gmail.com>
2020-09-01 21:09:56 -0500

    diseño home terminado

--- app/screens/Main.tsx
+++ app/screens/Main.tsx
@@ -21,30 +21,26 @@ export default class Main extends React.Component {
             activeIndex: 0,
             carouselItems: [
                 {
-                    title: "DoMusic",
-                    text: "Bienvenido",
-                    img: '../../assets/img1.jpg'
+                    title: "Bienvenido, acompañanos con tu armonia",
+                    imagen: "https://cdn.pixabay.com/photo/2015/05/15/14/50/concert-768722_960_720.jpg"
 
                 },
                 {
-                    title: "Item 2",
-                    text: "Text 2",
-                    img: '../../assets/img2.jpg'
+                    title: "Disfruta tu pasión por la música En cualquier momento y lugar",
+                    imagen: "https://cdn.pixabay.com/photo/2016/11/23/00/39/hands-1851500_960_720.jpg"
                 },
                 {
-                    title: "Item 3",
-                    text: "Text 3",
-                    img: '../../assets/img3.jpg'
+                    title: "Ábrete camino con tu musica, Demuestra tu talento",
+                    imagen: "https://cdn.pixabay.com/photo/2014/11/26/15/20/saxophone-546303_960_720.jpg"
+
                 },
                 {
-                    title: "Item 4",
-                    text: "Text 4",
-                    img: '../../assets/img4.jpg'
+                    title: "Vive la música, disfrútala en compañía",
+                    imagen: "https://cdn.pixabay.com/photo/2014/06/21/20/09/violin-374096_960_720.jpg"
                 },
                 {
-                    title: "Item 5",
-                    text: "Text 5",
-                    img: '../../assets/img5.jpg'
+                    title: "La musica eres Tu, Sigue tu Arminia",
+                    imagen: "https://cdn.pixabay.com/photo/2016/11/19/21/05/bass-guitar-1841186_960_720.jpg"
                 },
             ]
         }
@@ -53,13 +49,28 @@ export default class Main extends React.Component {
     _renderItem({ item, index }) {
         return (
             <View style={styles.head}>
-                
-                    <Image
-                        source={require('../../assets/img1.jpg')}
+                <Image
+                    style={styles.imagen}
+                    source={{
+                        width: 360,
+                        height: 480,
+                        uri: item.imagen,
+                    }}
+                />
+
+                <View style={styles.searchSection}>
+                    <Icon
+                        name='compact-disc'
+                        type='font-awesome-5'
+                        color='#092740'
+                        size={20}
+                        style={styles.icono}
                     />
-                
-                <Text style={{ fontSize: 30 }}>{item.title}</Text>
-                <Text>{item.text}</Text>
+                    <Text style={styles.logo}>DoMusic</Text>
+                </View>
+
+                <Text style={styles.title}>{item.title}</Text>
+
             </View>
 
 
@@ -75,10 +86,10 @@ export default class Main extends React.Component {
                 <View style={{ flex: 1, flexDirection: 'row', justifyContent: 'center', marginTop: 40 }}>
                     <Carousel
                         layout={'stack'}
-                        layoutCardOffset={18}
+                        layoutCardOffset={15}
                         ref={ref => this.carousel = ref}
                         data={this.state.carouselItems}
-                        sliderWidth={300}
+                        sliderWidth={280}
                         itemWidth={380}
                         renderItem={this._renderItem}
                         onSnapToItem={index => this.setState({ activeIndex: index })} />
@@ -94,15 +105,37 @@ const styles = StyleSheet.create({
     searchSection: {
         flexDirection: 'row',
         justifyContent: 'center',
-    },
+        textAlign: "center" 
+      },
 
     head: {
         alignItems: "center",
         marginLeft: 20,
         backgroundColor: 'white',
-        height: '90%',
-        borderRadius: 20,
-        padding: 20
+        height: '94%',
+        borderRadius: 23,
+
     },
 
+    imagen: {
+        borderTopLeftRadius: 19,
+        borderTopRightRadius: 19,
+    },
+    
+    logo: {
+        fontSize: 25,
+        fontWeight: "bold",
+        color: "#092740",
+        textAlign: "center"
+    },
+
+    title: {
+        fontSize: 25,
+        textAlign:"center",
+        color: "#092740",
+    },
+
+    icono:{
+        marginTop:10
+    }
 });
